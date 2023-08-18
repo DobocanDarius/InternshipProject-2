396490db58b8f0f557a7f707db4830d0b2338d24
CarlaTincu <carla.tincu@lateralgroup.com>
2023-08-18 13:26:50 +0300

    testing

---
+++ InternshipProject#2/Controllers/ControllerTesting.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace InternshipProject_2.Controllers
+{
+    public class ControllerTesting : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
