e632c8ba0cbcb4759a168e9ddebb6bc90618e0ba
Darius Dobocan <darius.dobocan@lateralgroup.com>
2023-08-18 15:39:07 +0300

    Login req/res

--- RequestResponseModels/User/Request/LoginRequest.cs
+++ RequestResponseModels/User/Request/LoginRequest.cs
@@ -4,9 +4,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace RequestResponseModels.User.Request
+namespace RequestResponseModels.User.Request;
+
+public class LoginRequest
 {
-    internal class LoginRequest
+    public LoginRequest(string username, string password, string email)
     {
+        Username = username;
+     Password = password;
+        Email = email;
     }
+    public string Username { get; set; } = null!;
+
+    public string Password { get; set; } = null!;
+
+    public string Email { get; set; } = null!;
 }
--- RequestResponseModels/User/Response/LoginResponse.cs
+++ RequestResponseModels/User/Response/LoginResponse.cs
@@ -1,12 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
-namespace RequestResponseModels.User.Response
+namespace RequestResponseModels.User.Response;
+
+public class LoginResponse
 {
-    internal class LoginResponse
-    {
-    }
+
+    public int Id { get; set; }
+
+    public string Username { get; set; } = null!;
+
+    public string Password { get; set; } = null!;
+
+    public string Email { get; set; } = null!;
+
+    public DateTime CreatedAt { get; set; }
 }
