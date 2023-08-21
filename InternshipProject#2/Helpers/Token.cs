23bcbbe038de363cfa559811da7236ebc45fa719
Darius Dobocan <darius.dobocan@lateralgroup.com>
2023-08-21 11:47:52 +0300

    .

--- InternshipProject#2/Helpers/Token.cs
+++ InternshipProject#2/Helpers/Token.cs
@@ -1,6 +1,28 @@
-﻿namespace InternshipProject_2.Helpers
+﻿using InternshipProject_2.Models;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace InternshipProject_2.Helpers;
+
+public class Token
 {
-    public class Token
+    private string Generate(User user)
     {
+        var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
+        var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+        var claims = new[]
+        {
+                new Claim("userId", user.Id.ToString()),
+                new Claim(ClaimTypes.Role,user.Role??"User"),
+            };
+        var token = new JwtSecurityToken(_config["Jwt:Issuer"],
+            _config["Jwt:Audience"],
+            claims,
+            expires: DateTime.Now.AddMinutes(5),
+            signingCredentials: credentials);
+
+        return new JwtSecurityTokenHandler().WriteToken(token);
     }
 }
