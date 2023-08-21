09f88a5a1b64a2f5f6a7bbae883b7643bcd5355d
Darius Dobocan <darius.dobocan@lateralgroup.com>
2023-08-21 13:30:16 +0300

    bug fix

--- InternshipProject#2/Helpers/Token.cs
+++ InternshipProject#2/Helpers/Token.cs
@@ -27,21 +27,7 @@ public class Token
             claims: claims,
             expires: DateTime.Now.AddMinutes(60),
             signingCredentials: credentials);
-    //private string Generate(User user)
-    //{
-    //    var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
-    //    var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
-    //    var claims = new[]
-    //    {
-    //            new Claim("userId", user.Id.ToString()),
-    //            new Claim(ClaimTypes.Role,user.Role??"User"),
-    //        };
-    //    var token = new JwtSecurityToken(_config["Jwt:Issuer"],
-    //        _config["Jwt:Audience"],
-    //        claims,
-    //        expires: DateTime.Now.AddMinutes(5),
-    //        signingCredentials: credentials);
 
-    //    return new JwtSecurityTokenHandler().WriteToken(token);
-    //}
+        return new JwtSecurityTokenHandler().WriteToken(token);
+    }
 }
