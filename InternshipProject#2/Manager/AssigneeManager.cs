44c06dae3eb9fb0ee8531017670b7dcfcf123e21
cata;om <catalinmoldovan02@gmail.com>
2023-08-21 10:28:15 +0300

    assignee manager

---
+++ InternshipProject#2/Manager/AssigneeManager.cs
@@ -0,0 +1,29 @@
+﻿using InternshipProject_2.Models;
+
+namespace InternshipProject_2.Manager
+{
+    public class AssigneeManager : IAssignmentManager
+    {
+        private Project2Context _dbContext;
+        public AssigneeManager()
+        {
+            _dbContext = new Project2Context();
+        }
+        public void AssignUserToTicket(int userId, int ticketId)
+        {
+            var assigment = new Assignee
+            {
+                UserId = userId,
+                TicketId = ticketId
+            };
+            _dbContext.Assignees.Add(assigment);
+            _dbContext.SaveChanges();
+        }
+
+        public int? GetAssignedUserIdForTicket(int ticketId)
+        {
+            var assignment = _dbContext.Assignees.Where(a=> a.TicketId == ticketId).Select(a => a.UserId).FirstOrDefault();
+            return assignment;
+        }
+    }
+}
---
+++ InternshipProject#2/Manager/IAssigneeManager.cs
@@ -0,0 +1,8 @@
+﻿namespace InternshipProject_2.Manager
+{
+    public interface IAssignmentManager
+    {
+        void AssignUserToTicket(int userId, int ticketId);
+        int? GetAssignedUserIdForTicket(int ticketId);
+    }
+}
