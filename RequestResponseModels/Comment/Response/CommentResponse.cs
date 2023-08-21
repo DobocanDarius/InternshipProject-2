2623ce3a1f862dd86987756a2e892a9d39e38bc4
CarlaTincu <carla.tincu@lateralgroup.com>
2023-08-21 09:36:34 +0300

    Comment request/response

---
+++ RequestResponseModels/Comment/Response/CommentResponse.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RequestResponseModels.Comment.Response
+{
+    public class CommentResponse
+    {
+        public CommentResponse(int Id, string Body, int UserId, int TickerId, DateTime CreatedAt, DateTime UpdatedAt) 
+        { 
+            this.Id = Id;
+            this.Body = Body;
+            this.UserId = UserId;
+            this.TicketId = TickerId;
+            this.CreatedAt = CreatedAt;
+            this.UpdatedAt = UpdatedAt;
+        }
+        public int Id;
+        public string Body;
+        public int UserId;
+        public int TicketId;
+        public DateTime CreatedAt;
+        public DateTime UpdatedAt;
+    }
+}
