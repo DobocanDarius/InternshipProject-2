3f4d4e9ed62e853a97f6e76233e75f62ef1bef86
catalinm28 <catalin.moldovan@lateralgroup.com>
2023-08-22 13:21:48 +0300

    adaugat request/response models

---
+++ RequestResponseModels/Assignee/Response/GetAssignedUserResponse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RequestResponseModels.Assignee.Response
+{
+    public class GetAssignedUserResponse
+    {
+        public string Username { get; set; }
+        public string Role { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+
+}
