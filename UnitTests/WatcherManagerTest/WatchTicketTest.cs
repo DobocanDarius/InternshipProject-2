c60ef4a63d81cef96aa9a2f88052ae8a0eafcd5b
Darius Dobocan <darius.dobocan@lateralgroup.com>
2023-08-24 14:23:08 +0300

    .

--- UnitTests/WatcherManagerTest/WatchTicketTest.cs
+++ UnitTests/WatcherManagerTest/WatchTicketTest.cs
@@ -1,12 +1,60 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using InternshipProject_2.Manager;
+using InternshipProject_2.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using RequestResponseModels.Watcher.Request;
+using RequestResponseModels.Watcher.Response;
 
-namespace UnitTests.WatcherManagerTest
+namespace UnitTests.WatcherManagerTest;
+
+[TestClass]
+public class WatchTicketTest
 {
-    internal class WatchTicketTest
+    private Project2Context _project2Context;
+    private WatcherManager _watcherManager;
+    private IConfiguration _configuration;
+
+    [TestInitialize]
+    public void Setup(IConfiguration configuration)
     {
+        _project2Context = new Project2Context();
+        _configuration = configuration;
+        _watcherManager = new WatcherManager(_project2Context, configuration);
+    }
+
+    [TestMethod]
+    public async Task WatchTicket()
+    {
+        //Arrange
+        var user = new User
+        {
+            Username = "Test",
+            Password = "password",
+            Email = "email",
+            Role = "developer",
+            CreatedAt = DateTime.Now
+
+        };
+        var ticket = new Ticket
+        {
+            Title = "Test",
+            Body = "Test",
+            Type = "Test",
+            Priority = "Test",
+            Component = "Test",
+            ReporterId = 1,
+            CreatedAt = DateTime.Now
+        };
+        _project2Context.Users.Add(user);
+        _project2Context.Tickets.Add(ticket);
+        _project2Context.SaveChanges();
+
+        var request = new WatchRequest(user.Id, ticket.Id);
+
+        //Act
+        WatchResponse result = await _watcherManager.WatchTicket(HttpContext, request);
+
+        //Assert
+        Assert.AreEqual("User assigned successfully", result.Message);
     }
 }
