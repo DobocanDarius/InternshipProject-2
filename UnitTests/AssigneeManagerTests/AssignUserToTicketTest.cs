cd62199817b2766bfde796f0080f6c97f3a7e9f8
Catalin Moldo <140596526+catalinm28@users.noreply.github.com>
2023-08-24 10:15:28 +0300

    unit test for each possible response (#13)
    * unit test for each possible response
    
    * unit test for every response
    
    * changed name for methods

--- UnitTests/AssigneeManagerTests/AssignUserToTicketTest.cs
+++ UnitTests/AssigneeManagerTests/AssignUserToTicketTest.cs
@@ -1,4 +1,5 @@
-﻿using InternshipProject_2.Manager;
+﻿using Azure;
+using InternshipProject_2.Manager;
 using InternshipProject_2.Models;
 using RequestResponseModels.Assignee.Request;
 using RequestResponseModels.Assignee.Response;
@@ -7,6 +8,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Collections.Specialized.BitVector32;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace UnitTests.AssigneeManagerTests
 {
@@ -24,8 +27,9 @@ namespace UnitTests.AssigneeManagerTests
         }
 
         [TestMethod]
-        public async Task AssignUserToTicket_ValidRequest_ReturnsSuccessResponse()
+        public async Task AssignUserToTicketValidRequest()
         {
+            //Arrange
             var user = new User
             {
                 Username = "Test",
@@ -54,9 +58,114 @@ namespace UnitTests.AssigneeManagerTests
                 TicketId = ticket.Id
             };
 
+            //Act
             AssignUserResponse result = await _assigneeManager.AssignUserToTicket(request);
 
+            //Assert
             Assert.AreEqual("User assigned successfully", result.Message);
+        }
+        [TestMethod]
+        public async Task AssignUserToTicketUserNotExist()
+        {
+            //Arrange
+            var request = new AssignUserRequest
+            {
+                UserId = 999,
+                TicketId = 2
+            };
+
+            //Act
+            AssignUserResponse result = await _assigneeManager.AssignUserToTicket(request);
+
+            //Assert
+            Assert.AreEqual("User not found", result.Message);
+        }
+        [TestMethod]
+        public async Task AssignUserToTicketUserNotDeveloper()
+        {
+            //Arrange
+            var user = new User
+            {
+                Username = "Test",
+                Password = "password",
+                Email = "email",
+                Role = "reporter",
+                CreatedAt = DateTime.Now
+            };
+            var ticket = new Ticket
+            {
+                Title = "Test",
+                Body = "Test",
+                Type = "Test",
+                Priority = "Test",
+                Component = "Test",
+                ReporterId = 1,
+                CreatedAt = DateTime.Now
+            };
+            _project2Context.Users.Add(user);
+            _project2Context.Tickets.Add(ticket);
+            _project2Context.SaveChanges();
+            var request = new AssignUserRequest
+            {
+                UserId = user.Id,
+                TicketId = ticket.Id
+            };
+
+            //Act
+            AssignUserResponse result = await _assigneeManager.AssignUserToTicket(request);
+
+            //Assert
+            Assert.AreEqual("User is not a developer", result.Message);
+        }
+
+        [TestMethod]
+        public async Task AssignUserToTicketAssignmentAlreadyExists()
+        {
+            // Arrange
+            var user = new User
+            {
+                Username = "Test",
+                Password = "password",
+                Email = "email",
+                Role = "developer",
+                CreatedAt = DateTime.Now
+            };
+            var ticket = new Ticket
+            {
+                Title = "Test",
+                Body = "Test",
+                Type = "Test",
+                Priority = "Test",
+                Component = "Test",
+                ReporterId = 1,
+                CreatedAt = DateTime.Now
+            };
+           
+
+            _project2Context.Users.Add(user);
+            _project2Context.Tickets.Add(ticket);
+            _project2Context.SaveChanges();
+            var existingAssignment = new Assignee
+            {
+                UserId = user.Id,
+                TicketId = ticket.Id
+            };
+            _project2Context.Assignees.Add(existingAssignment);
+            _project2Context.SaveChanges();
+
+            var request = new AssignUserRequest
+            {
+                UserId = user.Id,
+                TicketId = ticket.Id
+            };
+
+            // Act
+            AssignUserResponse result = await _assigneeManager.AssignUserToTicket(request);
+
+            // Assert
+            Assert.AreEqual("An assignment already exists for this ticket", result.Message);
         }
     }
 }
+
+
