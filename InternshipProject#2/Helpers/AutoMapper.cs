572fabb9924ae263b3274ffc6f13c311de9c36ec
DobocanDarius <140688584+DobocanDarius@users.noreply.github.com>
2023-08-24 10:27:48 +0300

    Deleted AutoMapper by mistake

---
+++ InternshipProject#2/Helpers/AutoMapper.cs
@@ -0,0 +1,36 @@
+﻿using AutoMapper;
+using InternshipProject_2.Models;
+using RequestResponseModels.Comment.Request;
+using RequestResponseModels.Comment.Response;
+using RequestResponseModels.Ticket.Request;
+using RequestResponseModels.Ticket.Response;
+using RequestResponseModels.User.Request;
+using RequestResponseModels.User.Response;
+using RequestResponseModels.Assignee.Request;
+using RequestResponseModels.Assignee.Response;
+
+namespace AutoMapper
+{
+    public class MapperConfig
+    {
+        public static Mapper InitializeAutomapper()
+        {
+            var config = new MapperConfiguration(cfg =>
+            {
+
+                cfg.CreateMap<User, CreateUserResponse>();
+                cfg.CreateMap<CreateUserRequest, User>();
+                cfg.CreateMap<CommentRequest, Comment>();
+                cfg.CreateMap<Comment, CommentResponse>();
+                cfg.CreateMap<Ticket, TicketResponse>();
+                cfg.CreateMap<TicketCreateRequest, Ticket>();
+                cfg.CreateMap<AssignUserRequest, Assignee>();
+                cfg.CreateMap<Assignee, AssignUserResponse>();
+                cfg.CreateMap<User, GetAssignedUserResponse>();
+            });
+
+            var mapper = new Mapper(config);
+            return mapper;
+        }
+    }
+}
