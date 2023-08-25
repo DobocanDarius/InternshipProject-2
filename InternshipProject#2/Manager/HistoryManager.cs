0cbe5c7a28ea0a2c479f9d744b6273d6c0bab979
catalinm28 <catalin.moldovan@lateralgroup.com>
2023-08-25 12:06:52 +0300

    ma gandesc ca create/edit/close nu are nevoie de validari decat daca ticket-ul exista

--- InternshipProject#2/Manager/HistoryManager.cs
+++ InternshipProject#2/Manager/HistoryManager.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using InternshipProject_2.Helpers;
 using InternshipProject_2.Models;
+using RequestResponseModels.History.Enum;
 using RequestResponseModels.History.Request;
 using RequestResponseModels.History.Response;
 
@@ -21,8 +22,36 @@ namespace InternshipProject_2.Manager
         {
             try
             {
-                var historyBody = _historyBodyGenerator.GenerateHistoryBody(request.EventType, request.UserId);
+                var user = await _dbContext.Users.FindAsync(request.UserId);
+                if (user == null)
+                {
+                    var response = new AddHistoryRecordResponse { Body = "User not found!" };
+                    return response;
+                }
+                var ticket = await _dbContext.Tickets.FindAsync(request.TicketId);
+                if(ticket == null)
+                {
+                    var response = new AddHistoryRecordResponse { Body = "Ticket not found!" };
+                }
 
+                if(request.EventType == HistoryEventType.Assign)
+                {
+                    var assignment = await _dbContext.Assignees.FindAsync(request.TicketId);
+                    if(assignment == null)
+                    {
+                        var response = new AddHistoryRecordResponse { Body = "Assignment not found!" };
+                    }
+                }
+                if(request.EventType == HistoryEventType.Comment)
+                {
+                    var comment = await _dbContext.Comments.FindAsync(request.TicketId,request.UserId);
+                    if(comment == null)
+                    {
+                        var response = new AddHistoryRecordResponse { Body = "Comment not found" };
+                        return response;
+                    }
+                }
+                var historyBody = _historyBodyGenerator.GenerateHistoryBody(request.EventType, request.UserId);
                 var historyRecord = map.Map<History>(request);
                 historyRecord.Body = historyBody;
                 historyRecord.CreatedAt = DateTime.Now;
