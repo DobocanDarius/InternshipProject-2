a4c72cd61e23e23ba8d4343d2d6b04b0e66c1568
catalinm28 <catalin.moldovan@lateralgroup.com>
2023-08-25 09:55:48 +0300

    adaugat unit test pentru celalat posibil raspuns, doar ca nu pot rula unit testurile

--- UnitTests/AssigneeManagerTests/RemoveAssignedUser.cs
+++ UnitTests/AssigneeManagerTests/RemoveAssignedUser.cs
@@ -66,6 +66,46 @@ namespace UnitTests.AssigneeManagerTests
             //Assert
             Assert.AreEqual("Assigned user removed successfully", resultFinal.Message);
         }
+        [TestMethod]
+        public async Task RemoveAssignedUser_NoAssignedUserFound()
+        {
+            // Arrange
+            var user = new User
+            {
+                Username = "Test",
+                Password = "password",
+                Email = "email",
+                Role = "developer",
+                CreatedAt = DateTime.Now
+            };
 
+            var ticket = new Ticket
+            {
+                Title = "Test",
+                Body = "Test",
+                Type = "Test",
+                Priority = "Test",
+                Component = "Test",
+                ReporterId = 1,
+                CreatedAt = DateTime.Now
+            };
+
+            _project2Context.Users.Add(user);
+            _project2Context.Tickets.Add(ticket);
+            _project2Context.SaveChanges();
+
+            var requestRemove = new RemoveAssignedUserRequest
+            {
+                TicketId = ticket.Id
+            };
+
+            // Act
+            RemoveAssignedUserResponse result = await _assigneeManager.RemoveAssignedUser(requestRemove);
+
+            // Assert
+            Assert.AreEqual("No assigned user found", result.Message);
+        }
     }
+
+}
 }
