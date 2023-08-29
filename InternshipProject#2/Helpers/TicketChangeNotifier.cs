bd024beb5fda1488d2287296947e0226346496fc
catalinm28 <catalin.moldovan@lateralgroup.com>
2023-08-29 14:07:48 +0300

    modificari

--- InternshipProject#2/Helpers/TicketChangeNotifier.cs
+++ InternshipProject#2/Helpers/TicketChangeNotifier.cs
@@ -34,30 +34,28 @@ namespace InternshipProject_2.BackgroundServices
                     var startTime = DateTime.UtcNow.AddMinutes(-5);
                     var endTime = DateTime.UtcNow;
 
-                    var tickets = await dbContext.Tickets.ToListAsync();
-                    Console.WriteLine("Exception");
-
-                    foreach (var ticket in tickets)
+                    var historyRecords = await historyManager.GetHistoryInTimeRange(startTime, endTime);
+                    var groupedHistory = historyRecords.GroupBy(hr => hr.TicketId);
+                    foreach (var group in groupedHistory)
                     {
-                        var historyRecords = await historyManager.GetHistoryInTimeRange(ticket.Id, startTime, endTime);
+                        var ticketId = group.Key;
+                        var historyRecordsForTicket = group.ToList();
+                        var emailBody = GenerateEmailContent(historyRecords);
+                        var watchers = await dbContext.Watchers
+                             .Where(watcher => watcher.TicketId == ticketId).ToListAsync();
 
-                        if (historyRecords.Count > 0)
-                        {
-                            var emailBody = GenerateEmailContent(historyRecords);
+                        var watcherUserIds = watchers.Select(watcher => watcher.UserId).ToList();
 
-                            var watchers = await dbContext.Watchers
-                            .Where(watcher => watcher.TicketId == ticket.Id).ToListAsync();
-                            var watcherUserIds = watchers.Select(watcher => watcher.UserId).ToList();
-                            var watcherEmails = await dbContext.Users
-                            .Where(user => watcherUserIds.Contains(user.Id))
-                            .Select(user => user.Email)
-                            .ToListAsync();
+                        var watcherEmails = await dbContext.Users
+                        .Where(user => watcherUserIds.Contains(user.Id))
+                        .Select(user => user.Email)
+                        .ToListAsync();
 
-                            await emailService.SendEmailAsync(watcherEmails, "Ticket Update Notification", emailBody);
-                        }
+                        await emailService.SendEmailAsync(watcherEmails, "Ticket Update Notification", emailBody);
+
                     }
                 }
-                // Wait for 5 minutes
+                
                 await Task.Delay(TimeSpan.FromMinutes(5), stoppingToken);
             }
         }
@@ -65,11 +63,10 @@ namespace InternshipProject_2.BackgroundServices
         {
             var formattedHistory = historyRecords.Select(record => $"{record.CreatedAt}: {record.Body}");
 
-            
             var emailContent = string.Join(Environment.NewLine, formattedHistory);
 
             return emailContent;
         }
+
     }
 }
-
