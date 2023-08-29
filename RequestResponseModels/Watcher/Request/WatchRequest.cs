572c11df132c1dcea6be87002504933c61b9216f
DobocanDarius <140688584+DobocanDarius@users.noreply.github.com>
2023-08-29 09:28:11 +0300

    Feature/watcher/watch ticket (#24)
    * .
    
    * Configure Manager
    
    * .
    
    * Configured Controller
    
    * ..
    
    * Merge with master
    
    * .
    
    * .
    
    * Updated program.cs
    
    * Re-did unit test
    
    *  Re-did unit test
    
    *  Re-did unit test
    
    * fixing unit test
    
    * interface update
    
    *  Re-did unit test
    
    * Re-did watcher manager and unit test
    
    * deleted unnecessary usings
    
    * Redid user validation

---
+++ RequestResponseModels/Watcher/Request/WatchRequest.cs
@@ -0,0 +1,14 @@
+﻿namespace RequestResponseModels.Watcher.Request;
+
+public class WatchRequest
+{
+    public WatchRequest(int userId, int ticketId)
+    {
+       UserId = userId;
+       TicketId = ticketId;
+    }
+    public int UserId { get; set; }
+
+    public int TicketId { get; set; }
+
+}
---
+++ UnitTests/WatcherManagerTest/WatchTicketTest.cs
@@ -0,0 +1,68 @@
+﻿using InternshipProject_2.Manager;
+using InternshipProject_2.Models;
+using RequestResponseModels.Watcher.Request;
+using RequestResponseModels.Watcher.Response;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class WatcherManagerTests
+    {
+        private WatcherManager _watchManager;
+        private Project2Context _dbContext;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _dbContext = new Project2Context();
+            _watchManager = new WatcherManager(_dbContext);
+        }
+
+        [TestMethod]
+        public async Task WatchTicket_Successful()
+        {
+            //Arrange
+            var user = new User
+            {
+                Username = "Test",
+                Password = "password",
+                Email = "email",
+                Role = "developer",
+                CreatedAt = DateTime.Now
+
+            };
+            var ticket = new Ticket
+            {
+                Title = "Test",
+                Body = "Test",
+                Type = "Test",
+                Priority = "Test",
+                Component = "Test",
+                ReporterId = 1,
+                CreatedAt = DateTime.Now
+            };
+            _dbContext.Users.Add(user);
+            _dbContext.Tickets.Add(ticket);
+            _dbContext.SaveChanges();
+            var request = new WatchRequest(user.Id, ticket.Id);
+
+            WatchResponse result = await _watchManager.WatchTicket(request, user.Id);
+
+            //Assert
+            Assert.AreEqual("Watching ticket", result.Message);
+        }
+
+        [TestMethod]
+        public async Task WatchTicket_AlreadyWatching()
+        {
+            //Arrange
+          
+            var request = new WatchRequest(2002, 1);
+
+            WatchResponse result = await _watchManager.WatchTicket(request, 1);
+
+            //Assert
+            Assert.AreEqual("Already watching ticket", result.Message);
+        }
+    }
+}
